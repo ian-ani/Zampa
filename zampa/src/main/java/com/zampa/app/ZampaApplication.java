a6d2aa392cf4c188783a3443058654187cdc165e
Iris <irisaunav@gmail.com>
2025-11-22 16:38:09 +0100

    Archivos iniciales por defecto de Spring

---
+++ zampa/src/main/java/com/zampa/app/ZampaApplication.java
@@ -0,0 +1,13 @@
+package com.zampa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZampaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZampaApplication.class, args);
+	}
+
+}
---
+++ zampa/src/test/java/com/zampa/app/ZampaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zampa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZampaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
