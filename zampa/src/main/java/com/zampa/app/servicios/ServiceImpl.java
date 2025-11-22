803bfe72f1230d38b1cc95083d0b46879d4f0efb
Iris <irisaunav@gmail.com>
2025-11-22 19:16:47 +0100

    Añadir detalleReceta a servicios

--- zampa/src/main/java/com/zampa/app/servicios/ServiceImpl.java
+++ zampa/src/main/java/com/zampa/app/servicios/ServiceImpl.java
@@ -1,14 +1,16 @@
+/* PAQUETE */
 package com.zampa.app.servicios;
 
+/* LIBRERIAS */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import lombok.extern.java.Log;
 
+/* LIBRERIAS INTERNAS */
 import com.zampa.app.modelos.Receta;
 import com.zampa.app.repositorios.RecetaRepository;
-
-import lombok.extern.java.Log;
 
 @Log
 @Service
@@ -20,6 +22,11 @@ public class ServiceImpl implements Servicio {
 	public Page<Receta> listadoRecetas(Pageable pageable) {
 		log.info(String.format("Se han pedido productos de la página %d con tamaño %d", pageable.getPageNumber(), pageable.getPageSize()));
 		return recetaRepository.findAll(pageable);
+	}
+
+	@Override
+	public Receta detalleReceta(Long id) {
+		return recetaRepository.findById(id).orElse(null);
 	}
 
 }
--- zampa/src/main/java/com/zampa/app/servicios/Servicio.java
+++ zampa/src/main/java/com/zampa/app/servicios/Servicio.java
@@ -1,12 +1,15 @@
 /* PAQUETE */
 package com.zampa.app.servicios;
 
+/* LIBRERIAS */
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-/* LIBRERIAS */
+/* LIBRERIAS INTERNAS */
 import com.zampa.app.modelos.Receta;
 
 public interface Servicio {
 	Page<Receta> listadoRecetas(Pageable pageable);
+
+	Receta detalleReceta(Long id);
 }
