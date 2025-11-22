5f9605bce6027f14867a212b0c23b1e561f1ba25
Iris <irisaunav@gmail.com>
2025-11-22 19:17:04 +0100

    Añadir ruta a detalle de cada receta

--- zampa/src/main/java/com/zampa/app/controladores/IndexController.java
+++ zampa/src/main/java/com/zampa/app/controladores/IndexController.java
@@ -1,11 +1,14 @@
+/* PAQUETE */
 package com.zampa.app.controladores;
 
+/* LIBRERIAS */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/* LIBRERIAS INTERNAS */
 import com.zampa.app.servicios.Servicio;
 
 @Controller
@@ -20,5 +23,12 @@ public class IndexController {
 		modelo.addAttribute("pagina", servicio.listadoRecetas(Pageable.ofSize(TAMANO_PAGINA)));
 		
 		return "index";
+	}
+	
+	@GetMapping("receta")
+	public String receta(Long id, Model modelo) {
+		modelo.addAttribute("receta", servicio.detalleReceta(id));
+		
+		return "receta";
 	}
 }
