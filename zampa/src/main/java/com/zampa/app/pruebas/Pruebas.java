3ef77fd8809d79d26d549d19cce7856a71d9a166
Iris <irisaunav@gmail.com>
2025-11-22 18:34:38 +0100

    Pruebas con dificultades

--- zampa/src/main/java/com/zampa/app/pruebas/Pruebas.java
+++ zampa/src/main/java/com/zampa/app/pruebas/Pruebas.java
@@ -8,12 +8,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import com.zampa.app.modelos.Dificultad;
 import com.zampa.app.modelos.Receta;
+import com.zampa.app.repositorios.DificultadRepository;
 import com.zampa.app.repositorios.RecetaRepository;
 import com.zampa.app.servicios.Servicio;
 
 @Component
 public class Pruebas implements CommandLineRunner {
+	private static final int TAMANO_PAGINA = 5;
+	
+	@Autowired
+	private DificultadRepository dificultadRepository;
+	
 	@Autowired
 	private RecetaRepository recetaRepository;
 	
@@ -22,14 +29,23 @@ public class Pruebas implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		recetaRepository.save(Receta.builder().nombre("Bizcocho de limón").build());	
-		recetaRepository.save(Receta.builder().nombre("Paella").build());
-		recetaRepository.save(Receta.builder().nombre("Tortilla").build());
+		/* DIFICULTADES */
+		Dificultad facil = dificultadRepository.save(Dificultad.builder().dificultad("Fácil").build());
+		Dificultad normal = dificultadRepository.save(Dificultad.builder().dificultad("Normal").build());
+		Dificultad dificil = dificultadRepository.save(Dificultad.builder().dificultad("Difícil").build());
+		
+		/* RECETAS */
+		recetaRepository.save(Receta.builder().nombre("Bizcocho de limón").descripcion("Se cogen dos huevos...").dificultad(facil).build());	
+		recetaRepository.save(Receta.builder().nombre("Paella").descripcion("Un vaso de arroz...").dificultad(dificil).build());
+		recetaRepository.save(Receta.builder().nombre("Tortilla").descripcion("Se cogen dos huevos...").dificultad(facil).build());
+		recetaRepository.save(Receta.builder().nombre("Pollo").descripcion("KFC momento").dificultad(facil).build());	
+		recetaRepository.save(Receta.builder().nombre("Yogur de frambuesa").descripcion("Leche?").dificultad(normal).build());
+		recetaRepository.save(Receta.builder().nombre("Sopa").descripcion("Sopa de pollo patrocionada por Mercadona").dificultad(normal).build());
 		
 		System.out.println("Repositorio con findAll(): ");
 		recetaRepository.findAll().forEach(System.out::println);
 		
-		var pageable = Pageable.ofSize(2);
+		var pageable = Pageable.ofSize(TAMANO_PAGINA);
 		
 		Page<Receta> pagina;
 		
