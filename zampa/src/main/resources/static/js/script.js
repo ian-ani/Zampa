2496f64a5cf0845575da8865217d2d90f4f39715
Iris <irisaunav@gmail.com>
2025-11-23 17:24:13 +0100

    Añadir botón de volver arriba

---
+++ zampa/src/main/resources/static/js/script.js
@@ -0,0 +1,20 @@
+/*
+** UTILIDADES
+*/
+
+/*  Botón de volver arriba */
+
+function setupBackToTop() {
+    const backToTop = document.getElementById("btt");
+
+    // Si el eje Y del scroll es superior a 200, anade una clase
+    window.addEventListener("scroll", () => {
+        (window.scrollY > 200) ? backToTop.classList.add("show") : backToTop.classList.remove("show");
+    });
+
+    backToTop.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    })
+}
+
+setupBackToTop();
