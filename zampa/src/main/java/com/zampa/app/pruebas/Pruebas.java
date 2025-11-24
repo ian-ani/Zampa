8c87c1c5bfee8e79c56bb41188d5fb8bb6d3db8b
Iris <irisaunav@gmail.com>
2025-11-24 14:27:29 +0100

    Validación pruebas si están vacía las tablas

--- zampa/src/main/java/com/zampa/app/pruebas/Pruebas.java
+++ zampa/src/main/java/com/zampa/app/pruebas/Pruebas.java
@@ -18,48 +18,55 @@ import com.zampa.app.servicios.Servicio;
 @Component
 public class Pruebas implements CommandLineRunner {
 	private static final int TAMANO_PAGINA = 10;
-	
+
 	@Autowired
 	private DificultadRepository dificultadRepository;
-	
+
 	@Autowired
 	private RecetaRepository recetaRepository;
-	
+
 	@Autowired
 	private Servicio servicio;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		/* DIFICULTADES */
-		Dificultad facil = dificultadRepository.save(Dificultad.builder().dificultad("Fácil").build());
-		Dificultad normal = dificultadRepository.save(Dificultad.builder().dificultad("Normal").build());
-		Dificultad dificil = dificultadRepository.save(Dificultad.builder().dificultad("Difícil").build());
-		
-		/* RECETAS */
-		recetaRepository.save(Receta.builder().nombre("Bizcocho de limón").descripcion("Se cogen dos huevos...").dificultad(facil).build());	
-		recetaRepository.save(Receta.builder().nombre("Paella").descripcion("Un vaso de arroz...").dificultad(dificil).build());
-		recetaRepository.save(Receta.builder().nombre("Tortilla").descripcion("Se cogen dos huevos...").dificultad(facil).build());
-		recetaRepository.save(Receta.builder().nombre("Pollo").descripcion("KFC momento").dificultad(facil).build());	
-		recetaRepository.save(Receta.builder().nombre("Yogur de frambuesa").descripcion("Leche?").dificultad(normal).build());
-		recetaRepository.save(Receta.builder().nombre("Sopa").descripcion("Sopa de pollo patrocionada por Mercadona").dificultad(normal).build());
-		
-		System.out.println("Repositorio con findAll(): ");
-		recetaRepository.findAll().forEach(System.out::println);
-		
-		var pageable = Pageable.ofSize(TAMANO_PAGINA);
-		
-		Page<Receta> pagina;
-		
-		do {
-			pagina = servicio.listadoRecetas(pageable);
-			
-			if (pagina.hasContent()) {
-				System.out.println("Página: ");
-				pagina.forEach(System.out::println);
-				
-				pageable = pagina.nextOrLastPageable();
-			}
-		} while (pagina.hasNext());
-	}
+		if (!dificultadRepository.findAll().iterator().hasNext()) {
+			/* DIFICULTADES */
+			Dificultad facil = dificultadRepository.save(Dificultad.builder().dificultad("Fácil").build());
+			Dificultad normal = dificultadRepository.save(Dificultad.builder().dificultad("Normal").build());
+			Dificultad dificil = dificultadRepository.save(Dificultad.builder().dificultad("Difícil").build());
 
+			/* RECETAS */
+			recetaRepository.save(Receta.builder().nombre("Bizcocho de limón").descripcion("Se cogen dos huevos...")
+					.dificultad(facil).build());
+			recetaRepository.save(
+					Receta.builder().nombre("Paella").descripcion("Un vaso de arroz...").dificultad(dificil).build());
+			recetaRepository.save(Receta.builder().nombre("Tortilla").descripcion("Se cogen dos huevos...")
+					.dificultad(facil).build());
+			recetaRepository
+					.save(Receta.builder().nombre("Pollo").descripcion("KFC momento").dificultad(facil).build());
+			recetaRepository.save(
+					Receta.builder().nombre("Yogur de frambuesa").descripcion("Leche?").dificultad(normal).build());
+			recetaRepository.save(Receta.builder().nombre("Sopa")
+					.descripcion("Sopa de pollo patrocionada por Mercadona").dificultad(normal).build());
+
+			System.out.println("Repositorio con findAll(): ");
+			recetaRepository.findAll().forEach(System.out::println);
+
+			var pageable = Pageable.ofSize(TAMANO_PAGINA);
+
+			Page<Receta> pagina;
+
+			do {
+				pagina = servicio.listadoRecetas(pageable);
+
+				if (pagina.hasContent()) {
+					System.out.println("Página: ");
+					pagina.forEach(System.out::println);
+
+					pageable = pagina.nextOrLastPageable();
+				}
+			} while (pagina.hasNext());
+		}
+	}
 }
